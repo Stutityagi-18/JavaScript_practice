a658df2a5fa9b26733f4c868315c4d620a0f37d6
Stutityagi-18 <stutityagi18@gmail.com>
2025-11-05 09:48:24 +0530

    Add 03_conversionOperations.js

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let score="33abc";
+console.log(typeof score);//number
+console.log(typeof (score));
+let valueinNumber=Number(score);
+console.log(typeof valueinNumber);
+console.log(valueinNumber)//so if we have score="33abc"then it will give NaN(Not a Number) otherwise if we have score="33" then it will give 33 as number
+//true=`1;false=0;
+let isloggedin=1
+
+let booleanisloggedin=Boolean(isloggedin);
+console.log(booleanisloggedin);//true
+//1==true
+//0==false
+//""==false
+//"stuti"==true
+let somenumber=33
+
+let stringnumber=String(somenumber);
+console.log(stringnumber);
+console.log(typeof stringnumber);
