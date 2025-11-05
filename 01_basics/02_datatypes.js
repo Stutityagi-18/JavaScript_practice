7ab6dfd8292d5a6705550a1a335c77b36704d647
Stutityagi-18 <stutityagi18@gmail.com>
2025-11-05 09:38:20 +0530

    Add 02_datatypes.js file

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,4 +3,21 @@
 // alert("hello")
 // alert(3+3) it will give error bcz we are using node.js not browser
 console.log(3+3) //it will print in terminal
-// onsole.log(3+3); console.log("Stuti") it is not readable,we should maintain code readability
+// console.log(3+3); console.log("Stuti") it is not readable,we should maintain code readability
+let name="Stuti"
+let age =19
+let isLoggedin=false;
+//number range => 2 to power 53
+//bigInt -- use when number is bigger in like trading and stocks
+//string ="" use with double quote
+//boolean=true/false
+//null=>standalone value
+//undefined=>value not assigned
+//symbol=>unique 
+//these were primitive data types
+
+
+//object=>
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined)//undefined
