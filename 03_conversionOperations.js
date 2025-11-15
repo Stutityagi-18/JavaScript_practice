a08bff8ed181abddfc0cfab2bd7acc00448a71be
Stutityagi-18 <stutityagi18@gmail.com>
2025-11-15 10:11:55 +0530

    updating 03_conversion

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -30,3 +30,14 @@ let str2="tyagi"
 let str3=str1+str2;
 console.log(str3)
 
+// console.log(1+"2") output will be 12 this is a problem we are adding number with a string
+// console.log("1"+2+2)  output will be 122,if string comes first then all the other will be treated as string
+ console.log(1+2+"3")//output will be 33
+ console.log(+true)//output will be 1 but it is not good practice
+ 
+ let num1,num2,num3;
+ num1=num2=num3=2+2//not a good practice
+ let gamecounter=100;
+ gamecounter++;
+ console.log(gamecounter);//output will be 101
+ 
