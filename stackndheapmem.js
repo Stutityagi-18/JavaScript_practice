8b476c031d08c7ab79435d73208ad43f64d0feb5
Stutityagi-18 <stutityagi18@gmail.com>
2025-11-19 19:07:22 +0530

    add file

---
+++ stackndheapmem.js
@@ -0,0 +1,23 @@
+// stack is used in primitive data types
+//heap is ued for non primitive data types
+
+let myutubename="stutityagi"//primitive data type let
+
+
+let anothername=myutubename
+
+anothername="codes";
+
+console.log(anothername);
+
+let user ={
+    email:"tyagi@gmail.com",
+    upi :"1233"
+}
+
+let anotheruser=user;
+
+anotheruser.email="stuti@gmail.com"
+
+console.log(user.email);
+console.log(anotheruser.email);
